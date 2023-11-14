98c81a911db4edbdff735568701ef44b553fa566
Ricky Roy Nardson <76934830+rickyroynardson@users.noreply.github.com>
2023-11-14 16:51:23 +0700

    feat(auth): create login (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import { logger } from "./utils/logger";
+import authController from "./auth/auth.controller";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use("/api/auth", authController);
 
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
