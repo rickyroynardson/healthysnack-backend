3347f953f8783d1097ebca3c629e849e5cee9fb8
Ricky Roy Nardson <rickyroynardson@gmail.com>
2023-12-06 20:43:24 +0700

    feat(product-material): add product material

--- src/product/product.type.ts
+++ src/product/product.type.ts
@@ -2,6 +2,12 @@ export interface CreateProductType {
   name: string;
   price: number;
   productCategoryId: number;
+  materials: {
+    name: string;
+    quantity: number;
+    unit: string;
+    price: number;
+  }[];
 }
 
 export interface UpdateProductType {
@@ -9,6 +15,13 @@ export interface UpdateProductType {
   price: number;
   stock: number;
   productCategoryId: number;
+  materials: {
+    materialId?: number;
+    name: string;
+    quantity: number;
+    unit: string;
+    price: number;
+  }[];
 }
 
 export interface ManageProductStockType {
--- src/product/product.validation.ts
+++ src/product/product.validation.ts
@@ -4,6 +4,16 @@ export const createProductValidation = z.object({
   name: z.string().min(3),
   price: z.number(),
   productCategoryId: z.number(),
+  materials: z
+    .array(
+      z.object({
+        name: z.string(),
+        quantity: z.number().min(1),
+        unit: z.string(),
+        price: z.number(),
+      })
+    )
+    .min(1, { message: "At least one material is required" }),
 });
 
 export const updateProductValidation = z.object({
@@ -11,6 +21,17 @@ export const updateProductValidation = z.object({
   price: z.number(),
   stock: z.number().min(0),
   productCategoryId: z.number(),
+  materials: z
+    .array(
+      z.object({
+        materialId: z.number().optional(),
+        name: z.string(),
+        quantity: z.number().min(1),
+        unit: z.string(),
+        price: z.number(),
+      })
+    )
+    .min(1, { message: "At least one material is required" }),
 });
 
 export const manageProductStockValidation = z.object({
